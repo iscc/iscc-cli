f08468e7a67d5013ba4dfaf5bbe75eca67a21810
Titusz Pan <titusz.pan@gmail.com>
2019-05-31 18:09:11 +0200

    Remove debug code.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,9 +9,7 @@ def pytest_addoption(parser):
 
 def pytest_unconfigure(config):
     if platform.system() == "Windows":
-        print("We are on Windows")
         terminate = config.getoption('--terminate')
         if terminate:
             import subprocess
-            print("Terminate Tika Java Server")
             subprocess.call("taskkill /F /T /IM java.exe")
