8b7c7e5f0a5a13da4ef8f4705a3fadcdacc3b1b9
Titusz Pan <titusz.pan@gmail.com>
2019-06-03 14:25:47 +0200

    Update tests and test data

--- tests/test_gen.py
+++ tests/test_gen.py
@@ -18,3 +18,14 @@ def test_gen_single_file():
     result = r.invoke(cli, ["gen", "tests/demo.jpg"])
     assert result.exit_code == 0
     assert "ISCC:CCTcjug7rM3Da" in result.output
+
+
+def test_gen_single_guess():
+    result = r.invoke(cli, ["gen", "tests/demo.txt"])
+    assert result.exit_code == 0
+    assert "ISCC:CTMjk4o5H96BV-CDhDNweoFxq8s-CRi62DsMaGxdk" in result.output
+    result = r.invoke(cli, ["gen", "-g", "tests/demo.txt"])
+    assert result.exit_code == 0
+    assert (
+        "ISCC:CCcdAr6GDoF3p-CTMjk4o5H96BV-CDhDNweoFxq8s-CRi62DsMaGxdk" in result.output
+    )
