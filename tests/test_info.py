b1c43ab3b8b8fb59d4574a7383aa2a33ebd5ec14
Titusz Pan <titusz.pan@gmail.com>
2019-06-03 14:03:13 +0200

    Command info shows supported formats and added tests for info command.

---
+++ tests/test_info.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import os
+from tests import ROOT_DIR
+from iscc_cli.cli import cli
+from click.testing import CliRunner
+
+os.chdir(ROOT_DIR)
+r = CliRunner()
+
+
+def test_info():
+    result = r.invoke(cli, ["info"])
+    assert result.exit_code == 0
+    assert "Supported File Types" in result.output
