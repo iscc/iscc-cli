194d79f70b3cfe0e18ecae9bb0de537bc4f05b2b
Titusz Pan <titusz.pan@gmail.com>
2019-06-07 16:00:35 +0200

    Add test for file stream.

--- tests/test_audio_id.py
+++ tests/test_audio_id.py
@@ -11,9 +11,19 @@ def test_content_id_audio():
     assert audio_id.content_id_audio([1, 2, 3]) == "CAFcywC1ZGJy9"
 
 
-def test_get_chroma_vector():
+def test_get_chroma_vector_file_path():
     if not fpcalc.is_installed():
         fpcalc.install()
     r = audio_id.get_chroma_vector("tests/demo.mp3")
     assert isinstance(r, list)
     assert r[0] == 684003877
+
+
+def test_get_chroma_vector_file_stream():
+    if not fpcalc.is_installed():
+        fpcalc.install()
+    with open("tests/demo.mp3", 'rb') as file_obj:
+        r = audio_id.get_chroma_vector(file_obj)
+    assert isinstance(r, list)
+    assert r[0] == 684003877
+
