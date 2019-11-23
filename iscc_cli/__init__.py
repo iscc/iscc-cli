2e034a75cc02a32b3ea586bd22a8fc1460de77c4
Titusz Pan <titusz.pan@gmail.com>
2019-11-23 11:36:39 +0100

    Set TIKA_PATH as environment variable.

--- iscc_cli/__init__.py
+++ iscc_cli/__init__.py
@@ -1,9 +1,10 @@
 import os
 import click
 
-os.environ["TIKA_VERSION"] = "1.22"
 __version__ = "0.7.0"
 
 APP_NAME = "iscc-cli"
 APP_DIR = click.get_app_dir(APP_NAME)
-TIKA_PATH = APP_DIR
+
+os.environ["TIKA_VERSION"] = "1.22"
+os.environ["TIKA_PATH"] = APP_DIR
