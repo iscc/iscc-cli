73f454d4d9979c785ed3e536d5789c7bf47d42f6
Titusz Pan <titusz.pan@gmail.com>
2019-11-23 12:43:47 +0100

    Add dump command.

--- iscc_cli/cli.py
+++ iscc_cli/cli.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import click
-from iscc_cli import __version__, init, gen, batch, sim, info, web
+from iscc_cli import __version__, init, gen, batch, sim, info, web, dump
 from click_default_group import DefaultGroup
 
 
@@ -16,6 +16,7 @@ cli.add_command(batch.batch)
 cli.add_command(web.web)
 cli.add_command(sim.sim)
 cli.add_command(info.info)
+cli.add_command(dump.dump)
 
 
 if __name__ == "__main__":
