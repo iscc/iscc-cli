5ba8a5e9e97bcf9ca811740869e1dc4297533f48
Titusz Pan <titusz.pan@gmail.com>
2019-12-22 19:49:42 +0100

    Don´t override TIKA_VERSION

--- iscc_cli/__init__.py
+++ iscc_cli/__init__.py
@@ -1,10 +1,9 @@
 import os
 import click
 
-__version__ = "0.8.1"
+__version__ = "0.8.2"
 
 APP_NAME = "iscc-cli"
 APP_DIR = click.get_app_dir(APP_NAME, roaming=False)
 
-os.environ["TIKA_VERSION"] = "1.22"
 os.environ["TIKA_PATH"] = APP_DIR
