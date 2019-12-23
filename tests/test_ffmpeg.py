359d8872a0898baea4930639a689bee8c7b5bef1
Titusz Pan <titusz.pan@gmail.com>
2019-12-23 15:09:13 +0100

    Add pytest ordering plugin

--- tests/test_ffmpeg.py
+++ tests/test_ffmpeg.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
 from iscc_cli import ffmpeg
-import pytest
 
 
 def test_exe_path():
@@ -12,20 +11,17 @@ def test_is_installed():
     assert isinstance(ffmpeg.is_installed(), bool)
 
 
-@pytest.mark.dependency()
 def test_download():
     out_path = ffmpeg.download()
     assert os.path.exists(out_path)
 
 
-@pytest.mark.dependency(depends=['test_download'])
 def test_install():
     exe_path = ffmpeg.install()
     assert os.path.exists(exe_path)
     assert ffmpeg.is_installed()
 
 
-@pytest.mark.dependency(depends=['test_install'])
 def test_get_version_info():
     vi = ffmpeg.get_version_info()
     assert vi.startswith("ffmpeg version 4.2.1")
