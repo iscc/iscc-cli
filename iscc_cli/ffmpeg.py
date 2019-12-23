25b21fe4d673002aef74712625cb04cdd568cb7d
Titusz Pan <titusz.pan@gmail.com>
2019-12-23 15:50:04 +0100

    Switch to imageio-ffmpeg.

--- iscc_cli/ffmpeg.py
+++ iscc_cli/ffmpeg.py
@@ -1,96 +1,18 @@
 # -*- coding: utf-8 -*-
 """A thin cross plattform installer and wrapper around ffmpeg."""
-import os
-import platform
-import shutil
-import subprocess
-import tarfile
-import zipfile
-import stat
-import click
-import iscc_cli
-from iscc_cli.utils import download_file
-
-
-FFMPEG_OS_MAP = {
-    "Linux": "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz",
-    "Darwin": "https://evermeet.cx/ffmpeg/ffmpeg-4.2.1.zip",
-    "Windows": "https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-4.2.1-win64-static.zip",
-}
-
-FFMPEG_MD5_MAP = {
-    "Linux": "d20e007536c6a7ab2ee63ea66c97472b",
-}
+import imageio_ffmpeg
 
 
 def exe_path():
     """Returns path to ffmpeg executable."""
-    if platform.system() == "Windows":
-        return os.path.join(iscc_cli.APP_DIR, "ffmpeg.exe")
-    return os.path.join(iscc_cli.APP_DIR, "ffmpeg")
-
-
-def is_installed():
-    """Check if ffmpeg is installed."""
-    fp = exe_path()
-    return os.path.isfile(fp) and os.access(fp, os.X_OK)
-
-
-def download_url():
-    """Return system specific download url"""
-    return FFMPEG_OS_MAP[platform.system()]
-
-
-def download():
-    """Download ffmpeg and return path to archive file"""
-    md5 = FFMPEG_MD5_MAP.get(platform.system(), None)
-    return download_file(download_url(), md5=md5)
-
-
-def extract(archive):
-    exe_file = os.path.basename(exe_path())
-    if archive.endswith(".zip"):
-        with zipfile.ZipFile(archive, "r") as zip_file:
-            for member in zip_file.namelist():
-                filename = os.path.basename(member)
-                if filename == exe_file:
-                    print(member)
-                    source = zip_file.open(member)
-                    target = open(exe_path(), "wb")
-                    with source, target:
-                        shutil.copyfileobj(source, target)
-    elif archive.endswith("tar.xz"):
-        with tarfile.open(archive, "r:xz") as tar_file:
-            for member in tar_file.getmembers():
-                if member.isfile() and member.name.endswith(exe_file):
-                    source = tar_file.extractfile(member)
-                    target = open(exe_path(), "wb")
-                    with source, target:
-                        shutil.copyfileobj(source, target)
-
-
-def install():
-    """Install fpcalc command line tool and retur path to executable."""
-    if is_installed():
-        click.echo("FFMPEG is already installed.")
-        return exe_path()
-    archive_path = download()
-    extract(archive_path)
-    st = os.stat(exe_path())
-    os.chmod(exe_path(), st.st_mode | stat.S_IEXEC)
-    assert is_installed()
-    return exe_path()
+    return imageio_ffmpeg.get_ffmpeg_exe()
 
 
 def get_version_info():
     """Get ffmpeg version info."""
-    try:
-        r = subprocess.run([exe_path(), "-version"], stdout=subprocess.PIPE)
-        return r.stdout.decode("utf-8").splitlines()[0].strip()
-    except Exception:
-        return ""
+    return imageio_ffmpeg.get_ffmpeg_version()
 
 
 if __name__ == "__main__":
-    print(install())
+    print(exe_path())
     print(get_version_info())
--- tests/test_ffmpeg.py
+++ tests/test_ffmpeg.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
 from iscc_cli import ffmpeg
 
 
@@ -7,21 +6,6 @@ def test_exe_path():
     assert "ffmpeg" in ffmpeg.exe_path()
 
 
-def test_is_installed():
-    assert isinstance(ffmpeg.is_installed(), bool)
-
-
-def test_download():
-    out_path = ffmpeg.download()
-    assert os.path.exists(out_path)
-
-
-def test_install():
-    exe_path = ffmpeg.install()
-    assert os.path.exists(exe_path)
-    assert ffmpeg.is_installed()
-
-
 def test_get_version_info():
     vi = ffmpeg.get_version_info()
-    assert vi.startswith("ffmpeg version 4.2.1")
+    assert vi.startswith("4.1")
