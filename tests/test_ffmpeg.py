5d1a6492ae88ee366e9acac6819e09d0eda85d84
Titusz Pan <titusz.pan@gmail.com>
2019-12-23 14:54:25 +0100

    Add test execution dependencies

--- tests/test_ffmpeg.py
+++ tests/test_ffmpeg.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 from iscc_cli import ffmpeg
+import pytest
 
 
 def test_exe_path():
@@ -11,17 +12,20 @@ def test_is_installed():
     assert isinstance(ffmpeg.is_installed(), bool)
 
 
+@pytest.mark.dependency()
 def test_download():
     out_path = ffmpeg.download()
     assert os.path.exists(out_path)
 
 
+@pytest.mark.dependency(depends=['test_download'])
 def test_install():
     exe_path = ffmpeg.install()
     assert os.path.exists(exe_path)
     assert ffmpeg.is_installed()
 
 
+@pytest.mark.dependency(depends=['test_install'])
 def test_get_version_info():
     vi = ffmpeg.get_version_info()
     assert vi.startswith("ffmpeg version 4.2.1")
