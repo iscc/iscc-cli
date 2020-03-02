4d91a676b73cd060a655a36b41276d15a08ad8df
Titusz Pan <titusz.pan@gmail.com>
2020-03-02 14:30:33 +0100

    Add mobi support for dump and batch commands

--- iscc_cli/dump.py
+++ iscc_cli/dump.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+import shutil
+
 import click
+import mobi
 from click import UsageError
-from tika import parser
+from tika import parser, detector
 from iscc_cli.utils import DefaultHelp
+from iscc_cli.const import SUPPORTED_MIME_TYPES
 import json
 
 
@@ -15,7 +19,19 @@ import json
 @click.option("-c", "--content", is_flag=True, default=False, help="Dump content only.")
 def dump(file, strip, meta, content):
     """Dump Tika extraction results for FILE."""
-    tika_result = parser.from_file(file.name)
+
+    media_type = detector.from_file(file.name)
+    if media_type not in SUPPORTED_MIME_TYPES:
+        click.echo("Unsupported media type {}.".format(media_type))
+        click.echo("Please request support at https://github.com/iscc/iscc-cli/issues")
+
+    if media_type == "application/x-mobipocket-ebook":
+        tempdir, epub_filepath = mobi.extract(file.name)
+        tika_result = parser.from_file(epub_filepath)
+        shutil.rmtree(tempdir)
+    else:
+        tika_result = parser.from_file(file.name)
+
     if all([meta, content]):
         raise UsageError("Use either --meta or --content for selective output.")
 
