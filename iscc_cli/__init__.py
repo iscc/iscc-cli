43cdd644c42590270f851244eff32678fdcb1d3b
Titusz Pan <titusz.pan@gmail.com>
2020-05-19 11:58:14 +0200

    Higher tika timeout.

--- iscc_cli/__init__.py
+++ iscc_cli/__init__.py
@@ -9,7 +9,7 @@ APP_DIR = click.get_app_dir(APP_NAME, roaming=False)
 os.makedirs(iscc_cli.APP_DIR, exist_ok=True)
 os.environ["TIKA_PATH"] = APP_DIR
 os.environ["TIKA_LOG_PATH"] = APP_DIR
-os.environ["TIKA_STARTUP_MAX_RETRY"] = "5"
+os.environ["TIKA_STARTUP_MAX_RETRY"] = "8"
 os.environ["LOGURU_AUTOINIT"] = "False"
 
 
