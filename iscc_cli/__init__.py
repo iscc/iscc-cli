dd53589b3c199f70867618ff3f7969372deee061
Titusz Pan <titusz.pan@gmail.com>
2021-11-16 17:45:00 +0100

    Update info command, bump version.

--- iscc_cli/__init__.py
+++ iscc_cli/__init__.py
@@ -1,6 +1,6 @@
 import os
 import click
 
-__version__ = "0.9.12"
+__version__ = "1.1.0b8"
 APP_NAME = "iscc-cli"
 APP_DIR = click.get_app_dir(APP_NAME, roaming=False)
--- iscc_cli/commands/info.py
+++ iscc_cli/commands/info.py
@@ -3,9 +3,13 @@ import click
 import iscc
 import iscc_cli
 import requests
+from iscc.bin import ffmpeg_version_info, ffprobe_version_info
+from iscc.audio import fpcalc_version_info
+from iscc.mediatype import SUPPORTED_EXTENSIONS
 
 
 def tika_version():
+    from tika import tika
     url = tika.ServerEndpoint + "/version"
     try:
         return requests.get(url).text
@@ -16,10 +20,12 @@ def tika_version():
 @click.command()
 def info():
     """Show information about environment."""
+    from tika import tika
     click.echo("ISCC Cli Version: %s" % iscc_cli.__version__)
     click.echo("ISCC Version: %s" % iscc.__version__)
-    click.echo("FFMPEG Version: %s" % ffmpeg.get_version_info())
-    click.echo("FPCALC Version: %s" % fpcalc.get_version_info())
+    click.echo("FFMPEG Version: %s" % ffmpeg_version_info())
+    click.echo("FFPROBE Version: %s" % ffprobe_version_info())
+    click.echo("FPCALC Version: %s" % fpcalc_version_info())
     click.echo("Tika Version: %s" % tika_version())
     click.echo("Tika Jar Path: %s" % tika.TikaJarPath)
     click.echo("Supported File Types: %s" % ", ".join(sorted(SUPPORTED_EXTENSIONS)))
