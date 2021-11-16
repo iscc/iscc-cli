0eba6076285ac00426429ffc85405ddf7d5ee26e
Titusz Pan <titusz.pan@gmail.com>
2021-11-17 00:29:44 +0100

    Update sim command.

--- iscc_cli/commands/sim.py
+++ iscc_cli/commands/sim.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-import sys
+import json
+
 import click
 import iscc
-from iscc_cli.utils import DefaultHelp, iscc_verify, iscc_split, iscc_clean
+from iscc_cli.utils import DefaultHelp
 
 
 @click.command(cls=DefaultHelp)
@@ -13,55 +14,8 @@ def sim(a, b):
 
     Example:
 
-        $ iscc sim CCUcKwdQc1jUM CCjMmrCsKWu1D
+        $ iscc sim AAA6P2X7C73P72Z4 AAAWTZWH76HZGAEF
 
     You may also compare fully qualified ISCC Codes with each other.
     """
-    try:
-        iscc_verify(a)
-        iscc_verify(b)
-    except ValueError as e:
-        click.echo(str(e))
-        sys.exit(1)
-
-    # Fully Qualified ISCC Code Similarity
-    avg_msg = None
-    if len(iscc_clean(a)) == 52 and len(iscc_clean(b)) == 52:
-        digest_a = b"".join(iscc.decode(code)[1:] for code in iscc_split(a))
-        digest_b = b"".join(iscc.decode(code)[1:] for code in iscc_split(b))
-        int_a = int.from_bytes(digest_a, "big", signed=False)
-        int_b = int.from_bytes(digest_b, "big", signed=False)
-        dist = bin(int_a ^ int_b).count("1")
-        similarity = ((192 - dist) / 192) * 100
-        avg_msg = "Average Estimated Similarity: {:.2f} % ({} of 192 bits differnt)".format(
-            similarity, dist
-        )
-
-    # Per Component Similarity
-    a = iscc_split(a)
-    b = iscc_split(b)
-
-    if len(a) == 1 and len(b) == 1:
-        type_a = ISCC_COMPONENT_CODES.get(a[0][:2])["name"]
-        type_b = ISCC_COMPONENT_CODES.get(b[0][:2])["name"]
-        if type_a != type_b:
-            click.echo("Incompatible component types ({} & {}).".format(type_a, type_b))
-
-    for ca in a:
-        for cb in b:
-            type_a = ISCC_COMPONENT_CODES.get(ca[:2])["name"]
-            type_b = ISCC_COMPONENT_CODES.get(cb[:2])["name"]
-            if type_a == type_b and type_a != "Instance-ID":
-                hamming_dist = iscc.distance(ca, cb)
-                hamming_sim = 64 - hamming_dist
-                similarity = round(hamming_sim / (2 * 64 - hamming_sim) * 100)
-                click.echo(
-                    "Estimated Similarity of {}: {:.2f} % ({} of 64 bits match)".format(
-                        type_a, similarity, hamming_sim
-                    )
-                )
-            if type_a == "Instance-ID" and type_b == "Instance-ID":
-                if ca == cb:
-                    click.echo("Identical Instance-ID")
-    if avg_msg:
-        click.echo(avg_msg)
+    click.echo(json.dumps(iscc.compare(a, b), indent=2))
