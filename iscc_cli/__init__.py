2b34c9f20586327bcdc0875cc7ce13681436fd5f
Titusz Pan <titusz.pan@gmail.com>
2021-11-17 23:48:28 +0100

    bump version to 1.1.0-beta.10

--- iscc_cli/__init__.py
+++ iscc_cli/__init__.py
@@ -1,7 +1,8 @@
 import os
 import click
 
-__version__ = "1.1.0b8"
+__version__ = "1.1.0b10"
+
 APP_NAME = "iscc-cli"
 APP_DIR = click.get_app_dir(APP_NAME, roaming=False)
 if not os.path.exists(APP_DIR):
