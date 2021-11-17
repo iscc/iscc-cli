a488667dde2ed059f9ceab78fab007115c44da6e
Titusz Pan <titusz.pan@gmail.com>
2021-11-17 01:30:08 +0100

    Update web command

--- iscc_cli/commands/web.py
+++ iscc_cli/commands/web.py
@@ -1,41 +1,28 @@
 # -*- coding: utf-8 -*-
+import json
 import os
-import shutil
 from io import BytesIO
 import click
 import iscc
-import mobi
 import requests
+from iscc.mediatype import SUPPORTED_MEDIATYPES
+from iscc.utils import download_file
+
 import iscc_cli
-from iscc_cli.utils import (
-    get_title,
-    mime_to_gmt,
-    DefaultHelp,
-    download_file,
-)
-from iscc_cli.mediatype import mime_guess, mime_clean
+from iscc_cli.utils import DefaultHelp
 
 HEADERS = {"User-Agent": "ISCC {}".format(iscc_cli.__version__)}
 
 
 @click.command(cls=DefaultHelp)
 @click.argument("url", type=click.STRING)
-@click.option(
-    "-g",
-    "--guess",
-    is_flag=True,
-    default=False,
-    help="Guess title (first line of text).",
-)
 @click.option("-t", "--title", type=click.STRING, help="Title for Meta-ID creation.")
 @click.option(
     "-e", "--extra", type=click.STRING, help="Extra text for Meta-ID creation."
 )
-@click.option("-v", "--verbose", is_flag=True, help="Enables verbose mode.")
-def web(url, guess, title, extra, verbose):
+@click.pass_context
+def web(ctx, url, title, extra):
     """Generate ISCC Code from URL."""
-
-    extra = extra or ""
 
     try:
         resp = requests.get(url, headers=HEADERS, stream=True)
@@ -43,64 +30,32 @@ def web(url, guess, title, extra, verbose):
         raise click.BadArgumentUsage(e)
 
     data = BytesIO(resp.content)
-    media_type = mime_clean(mime_guess(data))
-    if media_type not in SUPPORTED_MIME_TYPES:
+    media_type = iscc.mime_clean(iscc.mime_guess(data))
+    if media_type not in SUPPORTED_MEDIATYPES:
         click.echo("Unsupported media type {}".format(media_type))
         click.echo("Please request support at https://github.com/iscc/iscc-cli/issues")
         return
 
-    if media_type == "application/x-mobipocket-ebook":
-        data.seek(0)
-        tempdir, filepath = mobi.extract(data)
-        tika_result = parser.from_file(filepath)
-        shutil.rmtree(tempdir)
-    else:
-        data.seek(0)
-        tika_result = parser.from_buffer(data)
-
-    if not title:
-        title = get_title(tika_result, guess=guess, uri=url)
-
-    mid, norm_title, _ = iscc.meta_id(title, extra)
-    gmt = mime_to_gmt(media_type)
-    if gmt == GMT.IMAGE:
-        data.seek(0)
-        cid = iscc.content_id_image(data)
-    elif gmt == GMT.TEXT:
-        text = tika_result["content"]
-        if not text:
-            click.echo("Could not extract text")
-            return
-        cid = iscc.content_id_text(tika_result["content"])
-    elif gmt == GMT.AUDIO:
-        if not fpcalc.is_installed():
-            fpcalc.install()
-        data.seek(0)
-        features = audio_id.get_chroma_vector(data)
-        cid = audio_id.content_id_audio(features)
-    elif gmt == GMT.VIDEO:
-        local_path = download_file(url, sanitize=True)
-        features = video_id.get_frame_vectors(local_path)
-        cid = video_id.content_id_video(features)
+    local_path = download_file(url, sanitize=True)
+    try:
+        result = iscc.code_iscc(
+            local_path,
+            title=title,
+            extra=extra,
+            all_granular=ctx.obj.granular,
+            all_preview=ctx.obj.preview,
+        )
+    except ValueError as e:
+        raise click.ClickException(e)
+    finally:
         os.remove(local_path)
 
-    data.seek(0)
-    did = iscc.data_id(data)
-    data.seek(0)
-    iid, tophash = iscc.instance_id(data)
+    if ctx.obj.store:
+        ctx.obj.index.add(result)
 
-    if not norm_title:
-        iscc_code = "-".join((cid, did, iid))
-    else:
-        iscc_code = "-".join((mid, cid, did, iid))
+    if ctx.obj.unpack:
+        components = iscc.decompose(result["iscc"])
+        decomposed = "-".join([c.code for c in components])
+        result["iscc"] = decomposed
 
-    click.echo("ISCC:{}".format(iscc_code))
-
-    if verbose:
-        if norm_title:
-            click.echo("Norm Title: %s" % norm_title)
-        click.echo("Tophash:    %s" % tophash)
-        click.echo("Filepath:   %s" % url)
-        click.echo("GMT:        %s" % gmt)
-
-    return dict(iscc=iscc_code, norm_title=norm_title, tophash=tophash, gmt=gmt)
+    click.echo(json.dumps(result, indent=2))
