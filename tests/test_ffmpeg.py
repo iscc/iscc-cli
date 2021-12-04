43c731865896beedb0b1e8481f6d7081d5971059
Titusz Pan <titusz.pan@gmail.com>
2021-12-04 14:28:58 +0100

    Don´t skip ffmpeg tests on linux.

--- tests/test_ffmpeg.py
+++ tests/test_ffmpeg.py
@@ -1,33 +1,20 @@
 # -*- coding: utf-8 -*-
-import pytest
 import os
-import sys
 from iscc import bin
-
-
-def is_linux():
-    return sys.platform == "linux"
-
-
-def is_py36():
-    return sys.version.startswith("3.6")
 
 
 def test_exe_path():
     assert "ffmpeg" in bin.ffmpeg_bin()
 
 
-@pytest.mark.skipif(is_linux() and is_py36(), reason="custom ffmpeg")
 def test_ffmpeg_exists():
     assert os.path.exists(bin.ffmpeg_bin())
 
 
-@pytest.mark.skipif(is_linux() and is_py36(), reason="custom ffmpeg")
 def test_ffmpeg_executable():
     assert os.access(bin.ffmpeg_bin(), os.X_OK)
 
 
-@pytest.mark.skipif(is_linux() and is_py36(), reason="custom ffmpeg")
 def test_get_version_info():
     vi = bin.ffmpeg_version_info()
     assert vi.startswith("4.2")
