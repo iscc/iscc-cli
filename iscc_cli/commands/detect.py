0c02394975809e9c5847d9e723dfe9384a64c6eb
Titusz Pan <titusz.pan@gmail.com>
2021-12-12 21:27:19 +0100

    Fix detect command.

--- iscc_cli/commands/detect.py
+++ iscc_cli/commands/detect.py
@@ -14,12 +14,12 @@ def detect(ctx, file):
     header = file.read(4096)
     file.seek(0)
     result = dict(
-        mime_from_name=iscc.mime_from_name(file.name),
-        mime_from_data=iscc.mime_from_data(header),
+        mime_from_name=iscc.mediatype.mime_from_name(file.name),
+        mime_from_data=iscc.mediatype.mime_from_data(header),
     )
 
-    mg = iscc.mime_guess(file)
+    mg = iscc.mediatype.mime_guess(file)
     result["mime_guess"] = mg
-    result["gmt"] = iscc.mime_to_gmt(mg, file.name)
-    result["supported"] = iscc.mime_supported(mg)
+    result["gmt"] = iscc.mediatype.mime_to_gmt(mg, file.name)
+    result["supported"] = iscc.mediatype.mime_supported(mg)
     click.echo(json.dumps(result, indent=2))
